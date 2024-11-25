a658d926a9175dcaa3d50d2477a0d04c91442c55
Clément Pusterla-Marquette <clement.pusterla-marquette@lecole-ldlc.com>
2024-11-25 17:37:40 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="relative z-10 container mx-auto px-4">
         <div className="text-center mt-16 md:mt-0">
           <h1 className="text-4xl md:text-7xl font-bold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-forge-orange to-forge-red">
-            Forgez Votre PC Gaming Idéal
+            <br></br>Forgez Votre PC Gaming Idéal
           </h1>
           <p className="text-xl md:text-2xl text-gray-300 mb-8 max-w-3xl mx-auto">
             Création sur mesure, amélioration et maintenance de PC gaming par des experts passionnés

aa07496a7f44dd2a0434be71b7ead3584a51c1fa
Clément Pusterla-Marquette <clement.pusterla-marquette@lecole-ldlc.com>
2025-01-09 12:32:35 +0100

    Update PCConfigDetails.tsx

--- src/components/prebuilt/PCConfigDetails.tsx
+++ src/components/prebuilt/PCConfigDetails.tsx
@@ -6,7 +6,7 @@ import { priceRanges } from "@/pages/PreBuiltPCs";
 import { Card, CardContent } from "@/components/ui/card";
 
 const configComponents: { [key: string]: string[] } = {
-  "budget1": [
+  "thebeginning": [
     "AMD Ryzen 5 5600",
     "Stock AMD ventirad",
     "Gigabyte B550 GAMING X V2",
@@ -16,27 +16,27 @@ const configComponents: { [key: string]: string[] } = {
     "MSI MAG 100R",
     "MSI A650BN",
   ],
-    "budget2": [
+    "littleguy": [
     "AMD Ryzen 5 5600",
     "Be Quiet Pure Rock 2",
     "MSI B550-A PRO",
     "16 Go DDR4 3200 MHz Kingston Fury Beast",
     "WD_black SN770 1 To (NVMe)",
     "RX 6750 XT Speedster",
-    "MSI MAG A120 Airflow",
+    "MSI MAG 120A Airflow",
     "MSI A650BN",
   ],
-  "mid1": [
-    "AMD Ryzen 5 5600",
+  "viper": [
+    "AMD Ryzen 7 5700X",
     "MSI 360R V2",
     "ASUS ROG B550-F Gaming WiFi II",
     "16 Go DDR4 3200 MHz Patriot Viper Steel",
     "WD_black SN770 2 To (NVMe)",
-    "RTX 4060 Eagle 3X",
+    "RTX 4060 EAGLE OC",
     "Corsair 3000D Airflow",
     "ASUS Prime AP-750G",
   ],
-  "mid2": [
+  "airflowprime": [
     "AMD Ryzen 7 5800X",
     "MSI 360R V2",
     "ASUS ROG B550-F Gaming WiFi II",
@@ -46,9 +46,9 @@ const configComponents: { [key: string]: string[] } = {
     "Corsair 4000D Airflow",
     "ASUS Prime AP-750G",
   ],
-  "high1": [
+  "thehellbound": [
     "Intel Core i5-14600KF",
-    "Corsair H150i Elite Capellix XT White",
+    "arctic liquid freezer III Whtie",
     "ASUS Prime Z790-P WiFi",
     "32 Go DDR5 6000 MHz G.Skill Ripjaws M5 RGB",
     "WD_black SN770 2 To (NVMe)",
@@ -57,64 +57,64 @@ const configComponents: { [key: string]: string[] } = {
     "ASUS Prime AP-750G",
     "Configurations ventilée uniquement avec des RX120 LINK",
   ],
-  "high2": [
+  "infinity": [
     "Intel Core i5-14600KF",
     "MSI 360R V2",
-    "MSI MPG Z790 Edge Max WiFi",
+    "MSI MPG Z790 Edge TI Max WiFi",
     "32 Go DDR5 6000 MHz Lexar THOR RGB",
     "WD_black SN770 2 To (NVMe)",
-    "RTX 4070 Super Twin Edge Zotac",
+    "RTX 4070 super Zotac Twin Edge",
     "Lian Li O11 Dynamic Mini",
-    "Corsair RM1000X",
-    "Configurations ventilée uniquement avec des SL-120 Infinity",
+    "Corsair SF1000",
+    "Configurations ventilée uniquement avec des SL Infinity",
   ],
-  "extreme1": [
+  "tuffy": [
     "Intel Core i7-14700KF",
     "ASUS TUF LC II 360 ARGB",
     "Gigabyte Z790 AORUS Pro X WiFi 7",
     "32 Go DDR5 6000 MHz Lexar THOR RGB",
     "WD_black SN770 2 To (NVMe)",
-    "RTX 4070 Ti Super PNY Epic-X-RGB",
+    "RTX 4070 Ti super TUF GAMING OC",
     "Corsair 3000D Airflow",
     "ASUS TUF Gaming 1000G",
     "Configurations ventilée uniquement avec des TF120",
   ],
-  "extreme2": [
+  "thetaichi": [
     "AMD Ryzen 7 7800X3D",
-    "ASUS ROG LC III 360 ARGB",
-    "MSI MPG X670E Gaming Plus WiFi",
+    "ARCTIC Liquid Freezer III 360 (avec AF120)",
+    "MSI X670E GAMING PLUS WIFI",
     "32 Go DDR5 6000 MHz G.Skill Trident Z5 Neo RGB",
-    "KingStone KC3000 2 To + KC3000 1 To (NVMe)",
+    "KingStone KC3000 2 To (NVMe)",
     "RX 7900 XTX ASRock Taichi white",
     "Corsair 5000D RGB Airflow",
     "Corsair RM1000X",
-    "Configurations ventilée uniquement avec des RX120 LINK",
+    "Configurations ventilée uniquement avec des AF120",
   ],
-  "extreme3": [
+  "chillguy": [
     "Intel Core i9-14900KF",
     "Corsair iCUE Link TITAN 360 RGB",
     "Gigabyte Z790 AORUS Master X",
     "64 Go DDR5 6000 MHz G.Skill Trident Z5 Neo RGB",
-    "KingStone KC3000 2 To + KC3000 1 To (NVMe)",
+    "KingStone KC3000 2 To (NVMe)",
     "RTX 4080 Super PNY Epic-X-RGB",
     "Antec C8",
     "Corsair RM1000X",
     "Configurations ventilée uniquement avec des RX120 LINK",
   ],
-  "extreme4": [
+  "themaster": [
     "AMD Ryzen 7 9800X3D",
-    "ARCTIC Liquid Freezer III 360 A-RGB",
+    "ARCTIC Liquid Freezer III 360 (avec RX120)",
     "Gigabyte X870E AORUS MASTER",
     "64 Go DDR5 6000 MHz G.Skill Trident Z5 Neo RGB",
-    "Kingston KC3000 2 To x2 (NVMe)",
-    "ASRock Taichi OC Radeon RX 7900 XTX",
+    "Kingston KC3000 2 To (NVMe)",
+    "RX 7900 XTX Sapphire NITRO+",
     "Corsair iCUE LINK 6500X RGB",
     "Corsair RM1200x SHIFT",
     "Configurations ventilée uniquement avec des RX120 LINK",
   ],
-  "extreme5": [
+  "theoverkill": [
     "AMD Ryzen 7 9800X3D",
-    "Corsair iCUE Link TITAN 360 RGB",
+    "Corsair iCUE LINK TITAN 360 RX LCD",
     "ASUS ROG Crosshair X870E Hero",
     "64 Go DDR5 6000 MHz G.Skill Trident Z5 Neo RGB",
     "Samsung 990 Pro 4 To (NVMe)",
